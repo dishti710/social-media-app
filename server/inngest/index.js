2a05b46287e9b60da79f8c0fef78e0f83b13b68d
Dishti <dishtikaushik710@gmail.com>
2026-07-30 19:23:15 +0530

    error fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,9 +6,10 @@ export const inngest = new Inngest({ id: "pingup-app" });
 
 //Inngest function to save user data to a db
 const syncUserCreation=inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async ({event})=>{
+    {
+        id: 'sync-user-from-clerk',
+        triggers: { event: 'clerk/user.created' }
+    },    async ({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data;
         let username=email_addresses[0].email_address.split('@')[0];
 
@@ -31,12 +32,13 @@ const syncUserCreation=inngest.createFunction(
 
 //inngest function to update user data in a db
 const syncUserUpdation=inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
-    async ({event})=>{
+    {
+        id: 'update-user-from-clerk',
+        triggers: { event: 'clerk/user.updated' }
+    },    async ({event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data;
 
-        const userData={
+        const updatedUserData={
             email:email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture: image_url,
@@ -48,9 +50,10 @@ const syncUserUpdation=inngest.createFunction(
 
 //inngest function to delete user data in a db
 const syncUserDeletion=inngest.createFunction(
-    {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted'},
-    async ({event})=>{
+    {
+        id: 'delete-user-with-clerk',
+        triggers: { event: 'clerk/user.deleted' }
+    },    async ({event})=>{
         const {id }= event.data;
         await User.findByIdAndDelete(id);
     }
